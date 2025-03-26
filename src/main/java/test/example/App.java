75e1a6bc0c84324c24e27e123c9e5158ddb450d1
SunLeang123 <leangkiyan2020@gmail.com>
2025-03-26 10:59:08 +0700

    initialize

---
+++ src/main/java/test/example/App.java
@@ -0,0 +1,16 @@
+package test.example;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Simple Java Desktop App");
+            JLabel label = new JLabel("Hello from Java!", SwingConstants.CENTER);
+            frame.add(label);
+            frame.setSize(400, 300);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
